75674c7a3bc813bd90b6b66b97bf2db783a86e23
Christian Bingman <christianbingman@gmail.com>
2020-04-09 15:42:44 -0500

    Initial Commit

---
+++ Project6/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project6
+//
+//  Created by Christian Bingman on 4/9/20.
+//  Copyright © 2020 Christian Bingman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
